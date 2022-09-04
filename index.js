668f90921cc66da84d5c8ebebffc0ff95b0a5229
Kimber Herron <kh582013@gmail.com>
2022-09-04 14:58:55 -0400

    commit tree cb292cf4519d5c4d295bab29ae3b55628887017f

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Write your solution here!
+// Write your solutfuncion here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+  function destructivelyAppendCat(Ralph) {
+  cats.push('Ralph');
+  }
+
+  function destructivelyPrependCat(Bob) {
+    cats.unshift('Bob');
+  }
+  cats;
+ 
+  function destructivelyRemoveLastCat(Garfield) {
+    cats.pop();
+  }
+
+  function destructivelyRemoveFirstCat(Ralph) {
+    cats.shift();
+  }
+
+  function appendCat() {
+    const copyOfCats = [
+        ...cats,
+        'Broom'
+    ];
+    return copyOfCats;
+  }
+
+  function prependCat() {
+    const copyOfCats = [
+       "Arnold", ...cats
+    ];   
+    return copyOfCats;
+  }
+  function removeLastCat() {
+    const copyOfCats = 
+        cats.slice(0, -1)
+    ;
+    return copyOfCats;
+}
+function removeFirstCat() {
+    const copyOfCats =
+    cats.slice(1);
+    return copyOfCats;
+}
